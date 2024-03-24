361b16703fd215b994eb33ccb3a1627edcbf0f60
Fairies0feast <86023563+Fairies-Feast@users.noreply.github.com>
2024-03-24 09:45:10 -0500

    Keep caret position

--- editor.js
+++ editor.js
@@ -1,12 +1,16 @@
+var syntaxElement = document.getElementById("syntax");
+var textarea = document.getElementById("write");
 function convert(inputText) {
   return amsillaEditorProcess(inputText.replaceAll("&","&#38;").replaceAll("<","&lt;").replaceAll(">","&gt;").replaceAll("\n","<br>").replaceAll(" ","&nbsp;"));
 }
-var syntaxElement = document.getElementById("syntax");
-var textarea = document.getElementById("write");
 function highlight(){
+  var selectStart = textarea.selectionStart;
+  var selectEnd = textarea.selectionEnd;
   syntaxElement.innerHTML = convert(textarea.value);
   textarea.style.height = "auto";
   textarea.style.height = (textarea.scrollHeight) + "px";
+  textarea.selectionStart = selectStart;
+  textarea.selectionEnd = selectEnd;
 }
 textarea.addEventListener("input", highlight);
 highlight();
